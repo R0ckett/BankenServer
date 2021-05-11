343517456806de983f29e5029b75304c25821f6e
Marcus-design <62107435+Marcus-design@users.noreply.github.com>
2021-05-11 20:45:15 +0200

    Add project files.

---
+++ BankenServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Xml;
+using System.Net.Sockets;
+using System.IO;
+
+namespace BankenServer
+{
+    class Program
+    {
+        static TcpListener tcpListener;
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
